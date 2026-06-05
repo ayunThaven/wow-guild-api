bc4de0496f8380a1d02153e6f15db2581f686e14
daymondsama <gabin.daymond@gmail.com>
2026-06-05 18:11:09 +0200

    Clean projet de base

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,12 +1,8 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
 import { BlizzardModule } from './modules/blizzard/blizzard.module';
 import { AuthModule } from './modules/auth/auth.module';
 
 @Module({
   imports: [BlizzardModule, AuthModule],
-  controllers: [AppController],
-  providers: [AppService],
 })
 export class AppModule {}
